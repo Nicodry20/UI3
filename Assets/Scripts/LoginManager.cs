f93890d4b05ffbb978b60a7d74f86ad2c7e0361c
Nicodry20 <48178309@est.ort.edu.ar>
2024-05-27 15:51:50 -0300

    UI3 Password

---
+++ Assets/Scripts/LoginManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoginManager : MonoBehaviour
+{
+    public Button BtnAceptar;
+    public InputField inputPassword;
+
+    public string Password;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void CheckPassword()
+    {
+        if (inputPassword.text == Password)
+        {
+            Debug.Log("Access Granted");
+        }
+        else
+        {
+            Debug.Log("Access Denied");
+        }
+    }
+}
